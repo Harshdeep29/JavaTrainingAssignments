de45975d7b0d3eace66c716ce259f6deb9ed2070
Harshdeep Bishnoi <hdeep718@gmail.com>
2025-05-28 21:27:51 +0530

    Assignment 3 done

--- Module2/Assignment_03_ReverseWords/Assignment3.java
+++ Module2/Assignment_03_ReverseWords/Assignment3.java
@@ -1,4 +1,18 @@
 package Assignment_03_ReverseWords;
 
+import java.util.Scanner;
+
 public class Assignment3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String s = sc.nextLine();
+        String[] words = s.split(" ");
+        int length = words.length;
+        String reversed = "";
+        for (int i = length - 1; i >= 0; i--) {
+            reversed += words[i] + " ";
+        }
+        System.out.println(reversed);
+    }
 }
