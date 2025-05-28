780937d94c335f73cd5fd83281690175b2d67901
Harshdeep Bishnoi <hdeep718@gmail.com>
2025-05-28 21:14:54 +0530

    Assignment1 Module1

--- Module2/Assignment_01_ItemCostCalculator/Assignment1.java
+++ Module2/Assignment_01_ItemCostCalculator/Assignment1.java
@@ -1,4 +1,22 @@
 package Assignment_01_ItemCostCalculator;
 
 public class Assignment1 {
+    public static void main(String[] args) {
+
+        float itemPrice = 10.00f;
+        int numOfItemsAvailable = 1000;
+        float taxRate = 8.00f;
+        float discountPercentage = 10.00f;
+
+        int numOfItemsPurchased = 243;
+        float subtotal = itemPrice * numOfItemsPurchased;
+        float tax = subtotal * (taxRate/100);
+        float discount = (subtotal * (discountPercentage/100));
+        float total = subtotal + tax - discount;
+        System.out.println("Subtotal cost: " + subtotal);
+        System.out.println("Total tax: " + tax);
+        System.out.println("Total discount: " + discount);
+        System.out.println("Total total: " + total);
+
+    }
 }
